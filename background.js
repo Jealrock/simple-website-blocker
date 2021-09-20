e21e43e8d9a91debfe898355cb6a6904a6799373
Oleg Marakhovsky <oleg.marakhovsky@gmail.com>
2021-09-20 18:08:28 +0300

    Added on/off switch on browser action

--- background.js
+++ background.js
@@ -1,26 +1,43 @@
 let blockedHosts = [];
 let allowedPages = [];
+let isOn = true;
 
 function buildBlockedHosts(pages) {
   return [...new Set(pages.map(page => new URL(page).host))];
 }
 
-function onStorageGet(data) {
-  if (!data.allowedPages) return;
+function updateBrowserActionIcon() {
+  browser.browserAction.setIcon({
+    path: (isOn ? "on.svg" : "off.svg")
+  });
+}
 
-  allowedPages = data.allowedPages;
-  blockedHosts = buildBlockedHosts(allowedPages);
+function onStorageGet(data) {
+  if (data.allowedPages) {
+    allowedPages = data.allowedPages || [];
+    blockedHosts = buildBlockedHosts(allowedPages);
+  }
+
+  if (data.isOn) {
+    isOn = !!data.isOn;
+    updateBrowserActionIcon();
+  }
 }
 
 function onStorageUpdate(data) {
-  if (!data.allowedPages) return;
+  if (data.allowedPages) {
+    allowedPages = data.allowedPages.newValue || [];
+    blockedHosts = buildBlockedHosts(allowedPages);
+  }
 
-  allowedPages = data.allowedPages.newValue;
-  blockedHosts = buildBlockedHosts(allowedPages);
+  if (data.isOn) {
+    isOn = !!data.isOn.newValue;
+    updateBrowserActionIcon();
+  }
 }
 
 function onTabUpdate(tabId, changeInfo, tabInfo) {
-  if (!tabInfo.url) return;
+  if (!isOn || !tabInfo.url) return;
 
   const url = new URL(tabInfo.url);
   if (blockedHosts.indexOf(url.hostname) == -1) return;
@@ -29,6 +46,11 @@ function onTabUpdate(tabId, changeInfo, tabInfo) {
   browser.tabs.update({ url: browser.runtime.getURL("main.html") })
 }
 
+function onBrowserAction() {
+  browser.storage.local.set({ isOn: !isOn });
+}
+
 browser.storage.local.get(onStorageGet);
 browser.storage.onChanged.addListener(onStorageUpdate);
 browser.tabs.onUpdated.addListener(onTabUpdate, { properties: ["url"] });
+browser.browserAction.onClicked.addListener(onBrowserAction);
